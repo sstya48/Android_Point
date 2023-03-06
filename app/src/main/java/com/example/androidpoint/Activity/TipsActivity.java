ed865e5cebfbba2de6dd56a8f047edb180a5d3d0
dharti <dharati@dasinfomedia.com>
2023-03-06 09:45:39 +0530

    Tips design

---
+++ app/src/main/java/com/example/androidpoint/Activity/TipsActivity.java
@@ -0,0 +1,16 @@
+package com.example.androidpoint.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidpoint.R;
+
+public class TipsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tips);
+    }
+}

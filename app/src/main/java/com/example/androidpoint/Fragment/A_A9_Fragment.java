e2d4334dcb684bca5e3640128ae058ffc2f5d6fd
Mr.mori <sstya48@gmail.com>
2023-03-15 19:01:40 +0530

    image slider done

---
+++ app/src/main/java/com/example/androidpoint/Fragment/A_A9_Fragment.java
@@ -0,0 +1,25 @@
+package com.example.androidpoint.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.androidpoint.R;
+
+
+public class A_A9_Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_a__a9_, container, false);
+
+
+
+        return view;
+    }
+}

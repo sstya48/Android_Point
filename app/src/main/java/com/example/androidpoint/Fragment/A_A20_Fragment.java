ff83723f986ff30d76ec9c3572e2d5e8bd384c31
Mr.mori <sstya48@gmail.com>
2023-03-19 13:07:35 +0530

    basic design ready

--- app/src/main/java/com/example/androidpoint/Fragment/A_A20_Fragment.java
+++ app/src/main/java/com/example/androidpoint/Fragment/A_A20_Fragment.java
@@ -1,17 +1,30 @@
 package com.example.androidpoint.Fragment;
 
+import static android.content.Context.CAMERA_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.example.androidpoint.R;
 
 public class A_A20_Fragment extends Fragment {
 
+    private ToggleButton toggleFlashLightOnOff;
+    private CameraManager cameraManager;
+    private String getCameraID;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,7 +32,57 @@ public class A_A20_Fragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_a__a20_, container, false);
 
 
+ /*       toggleFlashLightOnOff = view.findViewById(R.id.toggle_flashlight);
 
+        cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+
+        try {
+            getCameraID = cameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        return view;
+    }
+
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public View toggleFlashLight(View view) {
+        if (toggleFlashLightOnOff.isChecked()) {
+            // Exception is handled, because to check
+            // whether the camera resource is being used by
+            // another service or not.
+            try {
+                // true sets the torch in ON mode
+                cameraManager.setTorchMode(getCameraID, true);
+
+                // Inform the user about the flashlight
+                // status using Toast message
+                Toast.makeText(getContext(), "Flashlight is turned ON", Toast.LENGTH_SHORT).show();
+            } catch (CameraAccessException e) {
+                // prints stack trace on standard error
+                // output error stream
+                e.printStackTrace();
+            }
+        } else {
+            // Exception is handled, because to check
+            // whether the camera resource is being used by
+            // another service or not.
+            try {
+                // true sets the torch in OFF mode
+                cameraManager.setTorchMode(getCameraID, false);
+
+                // Inform the user about the flashlight
+                // status using Toast message
+                Toast.makeText(getContext(), "Flashlight is turned OFF", Toast.LENGTH_SHORT).show();
+            } catch (CameraAccessException e) {
+                // prints stack trace on standard error
+                // output error stream
+                e.printStackTrace();
+            }
+        }
+*/
         return view;
     }
 }

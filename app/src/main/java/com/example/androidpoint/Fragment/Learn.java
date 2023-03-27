91624ec8175be4fe75339989a527f9344018ad30
Mr.mori <sstya48@gmail.com>
2023-03-28 00:09:18 +0530

    firebase first demo complete learn

--- app/src/main/java/com/example/androidpoint/Fragment/Learn.java
+++ app/src/main/java/com/example/androidpoint/Fragment/Learn.java
@@ -3,21 +3,43 @@ package com.example.androidpoint.Fragment;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.androidpoint.Adapter.LearnAdapter;
+import com.example.androidpoint.Model.LearnModel;
 import com.example.androidpoint.R;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Learn extends Fragment {
 
     AdView adView_learn;
+
+
+    private ArrayList<LearnModel> datalist;
+
+
+    private RecyclerView recyclerView;
+
+    FirebaseFirestore db;
+
 
 
     @SuppressLint("MissingInflatedId")
@@ -45,4 +67,38 @@ public class Learn extends Fragment {
 
         return view;
     }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        recyclerView= view.findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        datalist =new ArrayList<>();
+
+        LearnAdapter myAdapter=new LearnAdapter(getContext(),datalist);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(myAdapter);
+
+        db= FirebaseFirestore.getInstance();
+
+        db.collection("LearnAndroid").orderBy("title").get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        List<DocumentSnapshot> list= queryDocumentSnapshots.getDocuments();
+
+                        for (DocumentSnapshot d:list)
+                        {
+                            LearnModel learnModel=d.toObject(LearnModel.class);
+                            datalist.add(learnModel);
+                        }
+                        myAdapter.notifyDataSetChanged();
+
+                    }
+                });
+    }
+
 }

d7e70cf49df1ad683ee4f4cc9feb5bdf3ff2572f
sstya48 <sstya48@gmail.com>
2023-06-04 19:12:09 +0530

    OnBoard Click Effect, FlashLight Code Done.

--- app/src/main/java/com/example/androidpoint/Fragment/Advance/A_A20_Fragment.java
+++ app/src/main/java/com/example/androidpoint/Fragment/Advance/A_A20_Fragment.java
@@ -1,34 +1,59 @@
 package com.example.androidpoint.Fragment.Advance;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
-import android.hardware.camera2.CameraManager;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
-
-import androidx.appcompat.widget.AppCompatImageView;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ToggleButton;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
 import com.example.androidpoint.R;
 
-public class A_A20_Fragment extends Fragment {
+import java.io.IOException;
 
-    private ToggleButton toggleFlashLightOnOff;
-    private CameraManager cameraManager;
-    private String getCameraID;
+public class A_A20_Fragment extends Fragment implements SurfaceHolder.Callback {
 
     AppCompatImageView Btn_arrow;
 
+    Button onBtn, offBtn;
+    Camera camera;
+    android.hardware.Camera.Parameters parameters;
+    private boolean lightIsOn = false;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        SurfaceView preview = (SurfaceView) getView().findViewById(R.id.PREVIEW);
+        SurfaceHolder mHolder = preview.getHolder();
+        mHolder.addCallback(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        turnOffLight();
+    }
     @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_a__a20_, container, false);
 
-        Btn_arrow=view.findViewById(R.id.Btn_arrow);
+
+        offBtn = view.findViewById(R.id.offBtn);
+        onBtn = view.findViewById(R.id.onBtn);
+
+        Btn_arrow = view.findViewById(R.id.Btn_arrow);
 
         Btn_arrow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,58 +66,86 @@ public class A_A20_Fragment extends Fragment {
             }
         });
 
-
- /*       toggleFlashLightOnOff = view.findViewById(R.id.toggle_flashlight);
-
-        cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
-
-        try {
-            getCameraID = cameraManager.getCameraIdList()[0];
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA,}, PackageManager.PERMISSION_GRANTED);
         }
+        if (!getActivity().getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+            Toast.makeText(getActivity(), "Device has no flashlight", Toast.LENGTH_SHORT).show();
+        }
+
+
+        onBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                turnOnLight();
+            }
+        });
+        offBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                turnOffLight();
+            }
+        });
+
+
         return view;
     }
 
-
-
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    public View toggleFlashLight(View view) {
-        if (toggleFlashLightOnOff.isChecked()) {
-            // Exception is handled, because to check
-            // whether the camera resource is being used by
-            // another service or not.
-            try {
-                // true sets the torch in ON mode
-                cameraManager.setTorchMode(getCameraID, true);
-
-                // Inform the user about the flashlight
-                // status using Toast message
-                Toast.makeText(getContext(), "Flashlight is turned ON", Toast.LENGTH_SHORT).show();
-            } catch (CameraAccessException e) {
-                // prints stack trace on standard error
-                // output error stream
-                e.printStackTrace();
+    private void turnOnLight() {
+        if (!lightIsOn) {
+            if (camera == null || parameters == null) {
+                return;
             }
-        } else {
-            // Exception is handled, because to check
-            // whether the camera resource is being used by
-            // another service or not.
-            try {
-                // true sets the torch in OFF mode
-                cameraManager.setTorchMode(getCameraID, false);
+            parameters = camera.getParameters();
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(parameters);
+            camera.startPreview();
+            lightIsOn = true;
+        }
+    }
 
-                // Inform the user about the flashlight
-                // status using Toast message
-                Toast.makeText(getContext(), "Flashlight is turned OFF", Toast.LENGTH_SHORT).show();
-            } catch (CameraAccessException e) {
-                // prints stack trace on standard error
-                // output error stream
-                e.printStackTrace();
+    private void turnOffLight() {
+
+        if (lightIsOn) {
+            if (camera == null || parameters == null) {
+                return;
+            }
+
+            parameters = camera.getParameters();
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            camera.setParameters(parameters);
+            camera.stopPreview();
+            lightIsOn = false;
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (camera != null) {
+            camera.stopPreview();
+            camera.setPreviewCallback(null);
+            camera.release();
+            camera = null;
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (camera == null) {
+            camera = Camera.open();
+            parameters = camera.getParameters();
+            try {
+                camera.setPreviewDisplay(holder);
+            } catch (IOException e) {
+                camera.release();
+                camera = null;
             }
         }
-*/
-        return view;
     }
 }
